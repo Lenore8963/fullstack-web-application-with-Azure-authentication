0067bd3c6e7511160a2986e6554227e08aca6f8b
Lenore8963 <zhouchen8963@gmail.com>
2023-12-11 15:40:19 -0800

    first commit

---
+++ API/authConfig.js
@@ -0,0 +1,32 @@
+const passportConfig = {
+    credentials: {
+        tenantID: "a8eec281-aaa3-4dae-ac9b-9a398b9215e7",
+        clientID: "b6ffbd30-541d-46cd-bcd6-4c67dddaf04c"
+    },
+    metadata: {
+        authority: "login.microsoftonline.com",
+        discovery: ".well-known/openid-configuration",
+        version: "v2.0"
+    },
+    settings: {
+        validateIssuer: true,
+        passReqToCallback: true,
+        loggingLevel: "info",
+        loggingNoPII: true,
+    },
+    protectedRoutes: {
+        todolist: {
+            endpoint: "/api/todolist",
+            delegatedPermissions: {
+                read: ["Todolist.Read", "Todolist.ReadWrite"],
+                write: ["Todolist.ReadWrite"]
+            },
+            applicationPermissions: {
+                read: ["Todolist.Read.All", "Todolist.ReadWrite.All"],
+                write: ["Todolist.ReadWrite.All"]
+            }
+        }
+    }
+}
+
+module.exports = passportConfig;
---
+++ SPA/src/authConfig.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+export const msalConfig = {
+    auth: {
+        clientId: "6fdd1cb0-2d46-4e4b-b7be-60aef92394e5", // This is the ONLY mandatory field that you need to supply.
+        authority: "https://login.microsoftonline.com/a8eec281-aaa3-4dae-ac9b-9a398b9215e7", // Defaults to "https://login.microsoftonline.com/common"
+        redirectUri: "/", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+        postLogoutRedirectUri: "/", // Indicates the page to navigate after logout.
+        clientCapabilities: ["CP1"] // this lets the resource owner know that this client is capable of handling claims challenge.
+    },
+    cache: {
+        cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            /**
+             * Below you can configure MSAL.js logs. For more information, visit:
+             * https://docs.microsoft.com/azure/active-directory/develop/msal-logging-js
+             */
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                }
+            }
+        }
+    }
+};
+
+/**
+ * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const protectedResources = {
+    apiTodoList: {
+        endpoint: "http://localhost:5000/api/todolist",
+        scopes: {
+            read: [ "api://b6ffbd30-541d-46cd-bcd6-4c67dddaf04c/Todolist.Read" ],
+            write: [ "api://b6ffbd30-541d-46cd-bcd6-4c67dddaf04c/Todolist.ReadWrite" ]
+        }
+    }
+}
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+    scopes: [...protectedResources.apiTodoList.scopes.read, ...protectedResources.apiTodoList.scopes.write]
+};
